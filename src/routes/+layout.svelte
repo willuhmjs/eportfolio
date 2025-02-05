4dc37b1c26338e1ebdfeeb64eb284509db8115bf
William Faircloth <willuhmjs@gmail.com>
2025-02-05 15:12:59 -0500

    Update website styling and content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/about-me">About Me</a></li>
-        <li><a href="/blog">Blog</a></li>
         <li class="dropdown">
             <a href="#" class="dropbtn">Classes</a>
             <div class="dropdown-content">
@@ -16,6 +13,8 @@
 <style>
     :global(body) {
         font-family: Arial,ui-sans-serif,system-ui,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
+        background-color: #121212;
+        color: #e0e0e0;
     }
     nav {
         background-color: #333;
@@ -45,20 +44,20 @@
     nav ul li .dropdown-content {
         display: none;
         position: absolute;
-        background-color: #f9f9f9;
+        background-color: #333;
         min-width: 160px;
         box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
         z-index: 1;
     }
     nav ul li .dropdown-content a {
-        color: black;
+        color: #e0e0e0;
         padding: 12px 16px;
         text-decoration: none;
         display: block;
         text-align: left;
     }
     nav ul li .dropdown-content a:hover {
-        background-color: #f1f1f1;
+        background-color: #111;
     }
     nav ul li:hover .dropdown-content {
         display: block;
