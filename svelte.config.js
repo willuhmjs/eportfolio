4c80f7d128b760df9576e7767d33cf899a2b2830
willuhmjs <willuhmjs@gmail.com>
2025-03-16 23:30:32 -0400

    journal entry 1 and 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import relativeImages from "mdsvex-relative-images";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex({
+		remarkPlugins: [relativeImages],
 		layout: {
 			course: "src/lib/layouts/Course.svelte",
 			article: "src/lib/layouts/Article.svelte"
